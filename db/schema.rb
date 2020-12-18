0de109c101deffd0d114356acebe231fc7a23437
ssalinascalibar <ssalinascalibar@gmail.com>
2020-12-18 20:22:09 -0300

    create product model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_16_210642) do
+ActiveRecord::Schema.define(version: 2020_12_18_231524) do
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "photo"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
